efaa6b4eb247cea4f962a0b315b436cd8551f9f3
arobaseSuulei <souleymanediallo2004@outlook.com>
2025-01-12 00:42:53 +0100

    test

--- mon-projet/src/components/Card.jsx
+++ mon-projet/src/components/Card.jsx
@@ -38,7 +38,7 @@ export default function Cards() {
                            <Flex  gap="3" align="center">
                                <Avatar
                                    size="3"
-                                   src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                                   // src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
                                    radius="full"
                                    fallback="T"
                                />
