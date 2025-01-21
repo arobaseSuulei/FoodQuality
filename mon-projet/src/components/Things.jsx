e5057c9000bd9a9070bcb5a56aebaa820533be62
arobaseSuulei <souleymanediallo2004@outlook.com>
2025-01-21 19:32:58 +0100

    ok

--- mon-projet/src/components/Things.jsx
+++ mon-projet/src/components/Things.jsx
@@ -40,39 +40,45 @@ export default function Things() {
                 <Badge color={things[0]?.Healthiness === "Bon" ? "green" : "red"}>{things[0]?.Healthiness}</Badge>
 
 
-                <Box maxWidth="340px">
-                    <Card size="4">
-                        <Inset clip="padding-box" side="top" pb="current">
-                            <img
-                                //src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
-                                //alt="Bold typography"
-                                style={{
-                                    display: "block",
-                                    objectFit: "cover",
-                                    width: "100%",
-                                    height: 140,
-                                    backgroundColor: "var(--gray-5)",
-                                }}
-                            />
-                        </Inset>
-                        <div className={'flex gap-4 flex-col'}>
-                            <p>{things[0]?.Item}</p>
-                            <hr/>
-                            <nav className={'flex justify-between'}>
-                                <p>Category : </p>
-                                <p>{things[0]?.Category}</p>
-                            </nav>
-                            <nav className={'flex justify-between'}>
-                                <p>Category : </p>
-                                <p>{things[0]?.Category}</p>
-                            </nav>
-                            <nav className={'flex justify-between'}>
-                                <p>Category : </p>
-                                <p>{things[0]?.Category}</p>
-                            </nav>
-                        </div>
-                    </Card>
-                </Box>
+               <Text>
+                   <Box width={"450px"} maxWidth="940px">
+                       <Card size="4">
+                           <Inset clip="padding-box" side="top" pb="current">
+
+                           </Inset>
+                           <div className={'flex gap-4 flex-col'}>
+                               <p>{things[0]?.Item}</p>
+                               <hr/>
+                               <nav className={'flex justify-between'}>
+                                   <p>Category : </p>
+                                   <p>{things[0]?.Category}</p>
+                               </nav>
+                               <nav className={'flex justify-between'}>
+                                   <p>Protein : </p>
+                                   <p>{things[0]?.Protein}</p>
+                               </nav>
+                               <nav className={'flex justify-between'}>
+                                   <p>Sugar : </p>
+                                   <p className={'text-red-500'}>{things[0]?.Sugars}</p>
+                               </nav>
+                               <nav className={'flex justify-between'}>
+                                   <p>Protein : </p>
+                                   <p className={''}>{things[0]?.Protein}</p>
+                               </nav>
+                               <nav className={'flex justify-between'}>
+                                   <p>Vitamin A (%DailyValue) </p>
+                                   <p className={''}>{things[0]?.percentVitaminA}</p>
+                               </nav>
+                               <nav className={'flex justify-between'}>
+                                   <p>Total Fat : </p>
+                                   <p className={''}>{things[0]?.TotalFat}</p>
+                               </nav>
+
+
+                           </div>
+                       </Card>
+                   </Box>
+               </Text>
 
 
             </div>
